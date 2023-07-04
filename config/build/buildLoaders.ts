650b007a822f944f5501ffa54eb9016ebc3c3ba1
hiiam-andy <andrew.citb@gmail.com>
2023-07-04 19:24:13 +0300

    добавил подключение стилей

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,13 +2,25 @@ import webpack from "webpack";
 
 export function buildLoaders():webpack.RuleSetRule[]{
 
+
+  //если не TS, то нужен babel-loader
   const typescriptLoader =  {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   }
 
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      "style-loader", //создаёт "стайл" узлы из строк ДЖС
+      "css-loader",  //переводит css в коммонДЖС
+      "sass-loader", //компиллирует сасс в цсс
+    ],
+  }
+
   return [
-   typescriptLoader
+   typescriptLoader,
+   cssLoader,
   ]
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import "./Counter.scss";
 
 export default function Counter() {
   let [count, setCount] = useState(0);
 
   return (
     <div>
-      <button onClick={() => setCount(++count)}>+</button>
-      {count}
+      <button onClick={() => setCount(++count)}>ADD</button>
+      <h1>{count}</h1>
     </div>
   );
 }
