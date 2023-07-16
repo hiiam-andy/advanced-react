d8549fec479df5b1153874e442797582898a0b56
hiiam-andy <andrew.citb@gmail.com>
2023-07-16 11:34:52 +0300

    организовал архитектуру feature sliced

--- src/app/providers/themeProvider/lib/useTheme.ts
+++ src/app/providers/themeProvider/lib/useTheme.ts
@@ -1,12 +1,12 @@
 import { useContext } from "react";
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "../lib/ThemeContext";
 
 interface UseThemeRerult {
   toggleTheme: ()=>void;
   theme: Theme
 }
 
-export default function useTheme(): UseThemeRerult {
+export function useTheme(): UseThemeRerult {
   const { theme, setTheme } = useContext(ThemeContext);
   const toggleTheme = () => {
     const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK
