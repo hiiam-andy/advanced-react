3e78a9f8084cc9a96194cc8dee4ad907d13b4317
hiiam-andy <andrew.citb@gmail.com>
2023-10-01 11:45:50 +0300

    подключил jest, провёл тест

--- src/shared/lib/classNames/ClassNames.ts
+++ src/shared/lib/classNames/ClassNames.ts
@@ -4,10 +4,24 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
   return [
     cls,
     ...additional.filter(Boolean),
-    Object.entries(mods)
+    ...Object.entries(mods)
       .filter(([_, value]) => Boolean(value))
       .map(([className, _]) => className),
   ].join(' ');
 }
+
+// type Mods = Record<string, boolean | string>
+
+// export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
+//     return [
+//         cls,
+//         ...additional.filter(Boolean),
+//         ...Object.entries(mods)
+//             .filter(([_, value]) => Boolean(value))
+//             .map(([className]) => className),
+//     ]
+//         .join(' ');
+// }
+
 
 classNames('remove-btn', { hovered: false, selectable: true }, ['pbg']);
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-export function test(arg: number): string {
-  console.log('yep фуау234фаау');
-  return '';
-}
