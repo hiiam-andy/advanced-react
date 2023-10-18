8c57d383b3e690d95a216c462420fd867dc82dcd
hiiam-andy <andrew.citb@gmail.com>
2023-10-18 17:53:08 +0300

    начал настройку CI

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -6,11 +6,15 @@
 import path from 'path';
 
 export default {
+  // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
+  // The test environment that will be used for testing
   testEnvironment: 'jsdom',
+  // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
     '\\\\node_modules\\\\',
   ],
+  // An array of file extensions your modules use
   moduleFileExtensions: [
     'js',
     'jsx',
@@ -19,16 +23,18 @@ export default {
     'json',
     'node',
   ],
+  // An array of directory names to be searched recursively up from the requiring module's location
   moduleDirectories: [
     'node_modules',
   ],
   modulePaths: [
     '<rootDir>src',
   ],
+  // The glob patterns Jest uses to detect test files
   testMatch: [
-    // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
     '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
   ],
+  // The root directory that Jest should scan for tests and modules within
   rootDir: '../../',
   setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
   moduleNameMapper: {
