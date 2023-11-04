102778c4072a7f29a7114ebea5452eee396408d0
hiiam-andy <andrew.citb@gmail.com>
2023-11-04 12:49:44 +0300

    добавил отчёт локи

--- src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import { classNames } from 'shared/lib/classNames/сlassNames';
-import { ThemeSwitcher } from 'shared/ui/themeSwitcher';
 import { LangSwitcher } from 'shared/ui/langSwitcher/LangSwitcher';
 import { Button, ButtonSize, ThemeButton } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
@@ -8,6 +6,8 @@ import { AppLink, AppLinkTheme } from 'shared/ui/appLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/RouteConfig';
 import MainIcon from 'shared/assets/icons/main-20-20.svg';
 import AboutIcon from 'shared/assets/icons/about-20-20.svg';
+import { classNames } from 'shared/lib/classNames/сlassNames';
+import { ThemeSwitcher } from '../../../../shared/ui/themeSwitcher/index';
 import styles from './Sidebar.module.scss';
 
 interface SidebarProps {
