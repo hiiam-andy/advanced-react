b888fed27fa8644ac29837390a966ad640523403
hiiam-andy <andrew.citb@gmail.com>
2023-11-05 16:01:59 +0300

    настроил Redux Toolkit

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,14 @@
-// import { BugButton } from 'app/providers/errorBoundary';
+import { BugButton } from 'app/providers/errorBoundary';
+import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
 
 export default function MainPage() {
   const { t } = useTranslation('main');
   return (
     <div>
-      {/* <BugButton /> */}
+      <BugButton />
       {t('Главная страница')}
+      <Counter />
     </div>
   );
 }
